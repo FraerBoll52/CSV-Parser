3c6c85710daa4443929ce14aa389bcc9d6621ab4
FraerBoll52 <kzeltov110@gmail.com>
2025-08-10 21:29:45 +0400

    Testing out parser

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,20 @@
 package org.example;
 
 
+import com.opencsv.exceptions.CsvException;
+
+import java.io.IOException;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            List<Person> people = CsvParser.parse("/Users/kirillzeltov/IdeaProjects/CSV Parser/src/main/resources/valid_data.csv");
+            people.forEach(System.out::println);
+        } catch (IOException | CsvException e) {
+            System.err.println("Ошибка парсинга CSV: " + e.getMessage());
+        }
+
 
         }
     }
